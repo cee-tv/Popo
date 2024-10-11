1f1248ad571f30cd18b4467bb74b7499a08f175c
Jayce <kendump666@gmail.com>
2024-10-11 16:53:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1332,3 +1332,23 @@ var playerInstance = jwplayer("jwplayerDiv");
         window.location.href = "https://telegram.me/demo";
       }
     });
+
+(function () {
+        (function a() {
+            try {
+                (function b(i) {
+                    if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+                        (function () { }).constructor('debugger')()
+                    } else {
+                        debugger
+                    }
+                    b(++i)
+                }
+                )(0)
+            } catch (e) {
+                setTimeout(a, 5000)
+            }
+        }
+        )()
+    }
+    )();
